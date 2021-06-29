849d2e45989991537581d8261df19059629f2ef1
patrickjquinn <patrick.jm.quinn@gmail.com>
2021-06-29 09:33:40 +0100

    Add deps

---
+++ src/components/Editor.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+    import type monaco from 'monaco-editor'
+    import { onMount } from 'svelte'
+    import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker'
+    import jsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker'
+    import cssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker'
+    import htmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker'
+    import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker'
+
+    let divEl: HTMLDivElement = null
+    let editor: monaco.editor.IStandaloneCodeEditor
+    let Monaco
+
+    onMount(async () => {
+        // @ts-ignore
+        self.MonacoEnvironment = {
+            getWorker: function (_moduleId: any, label: string) {
+                if (label === 'json') {
+                    return new jsonWorker()
+                }
+                if (label === 'css' || label === 'scss' || label === 'less') {
+                    return new cssWorker()
+                }
+                if (label === 'html' || label === 'handlebars' || label === 'razor') {
+                    return new htmlWorker()
+                }
+                if (label === 'typescript' || label === 'javascript') {
+                    return new tsWorker()
+                }
+                return new editorWorker()
+            }
+        }
+
+        Monaco = await import('monaco-editor')
+        editor = Monaco.editor.create(divEl, {
+            value: ['function x() {', '\tconsole.log("Hello world!")', '}'].join('\n'),
+            language: 'typescript'
+        })
+
+        return () => {
+            editor.dispose()
+        }
+    })
+</script>
+
+<div bind:this={divEl} class="h-screen" />
---
+++ src/views/MainBuilder.svelte
@@ -0,0 +1,11 @@
+<script lang='ts'>
+
+</script>
+
+<main>
+
+</main>
+
+<style>
+    
+</style>
