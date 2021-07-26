5d16e4ca4317158afc78adeca689f28d1190799e
Pat Quinn <patrick.jm.quinn@gmail.com>
2021-07-26 19:02:28 +0100

    Added nobby as a dep and added agnostic draggable as a dep to full flesh out the UI builder

---
+++ src/components/LayoutEditor.svelte
@@ -0,0 +1,46 @@
+<script>
+	import * as knobby from 'svelte-knobby'
+	import { createEventDispatcher } from 'svelte'
+
+	const dispatch = createEventDispatcher()
+
+	function layoutUpdated() {
+		dispatch('message', layoutProperties)
+	}
+
+    $: {
+        layoutUpdated()
+    }
+
+	export let layoutProperties = {
+		message: 'Hello World!',
+		color: '#ff3e00',
+		clicks: 0,
+		checked: false,
+
+		increment: (value) => ({
+			...value,
+			clicks: value.clicks + 1
+		}),
+
+		constrained: {
+			$label: 'labelled input',
+			value: 50,
+			min: 0,
+			max: 100,
+			step: 1
+		},
+
+		folder: {
+			$label: 'labelled folder',
+			a: 1,
+			b: 2,
+			nested: {
+				c: 3,
+				d: 4
+			}
+		}
+	}
+
+	const controls = knobby.panel(layoutProperties)
+</script>
